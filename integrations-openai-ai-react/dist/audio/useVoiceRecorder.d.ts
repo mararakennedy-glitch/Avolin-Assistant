0c1033a2bbffad7909fde5153c9a65e2579f3865
mararakennedy-glitch <mararakennedy@gmail.com>
2026-06-27 08:43:47 +0200

    Delete integrations-openai-ai-react directory

--- integrations-openai-ai-react/dist/audio/useVoiceRecorder.d.ts
+++
@@ -1,7 +0,0 @@
-export type RecordingState = "idle" | "recording" | "stopped";
-export declare function useVoiceRecorder(): {
-    state: RecordingState;
-    startRecording: () => Promise<void>;
-    stopRecording: () => Promise<Blob>;
-};
-//# sourceMappingURL=useVoiceRecorder.d.ts.map
